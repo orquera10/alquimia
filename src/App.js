0199370025b5c803cf7cc61834cf6a76b859b545
Dario <orquera10@gmail.com>
2023-05-08 20:38:50 -0300

    estableciendo estructura del proyecto alquimia

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Error404 from "./components/Error404";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Darioooooo</h1>
-    </div>
+    <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path={"/"} element={<HomeContainer />} />
+            <Route path={"/servicios"} element={<ServiceContainer />} />
+            <Route path={"/trabajos"} element={<WorkContainer />} />
+            <Route path={"/contacto"} element={<Contact/>} /> 
+            <Route path={"/nosostros"} element={<AboutUs />} />
+            <Route path={"*"} element={<Error404 />} />           
+          </Routes>
+          <Footer />
+    </BrowserRouter>
   );
 }
 

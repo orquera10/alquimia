f8d8416649cf1222ee0c82fe444135a08962421f
Melanie Gerenni <mgerenni@gmail.com>
2023-06-02 00:29:47 -0300

    optimizando el componente ImgContainer

--- src/components/ImgWork/ImgContainer.jsx
+++ src/components/ImgWork/ImgContainer.jsx
@@ -5,22 +5,12 @@ const ImgContainer = (props) => {
   const {data} = props;
   const {images, description, state} = data;
 
-
-  if (state==="active") {
     return (
-      <div className="col col-12 card-work">
+      <div className={`col col-12 card-work ${!state ? " col-md-6" : ""}`}>
         <Carrusel controls={true} array={images} />
         <p className="text-center mt-3 fw-normal">{description}</p>
       </div>
       );
-  } else {
-    return (
-      <div className="col col-12 col-md-6 card-work">
-        <Carrusel controls={true} array={images} />
-        <p className="text-center mt-3 fw-normal">{description}</p>
-      </div>
-      );
-  }
   
 };
 
